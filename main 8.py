c0a7f41ce6f31885934ca2f15d8422062f105dcf
lreigaud <lreigaud@gmail.com>
2024-11-22 11:00:07 +0100

    Add files via upload
    Exercices solved during class time 
    Exercices réalisés en cours

---
+++ main 8.py
@@ -0,0 +1,26 @@
+#11 p 97
+
+"""
+def valeur(x) :
+  x=0
+  while x**2<2:
+    x=x+0.01
+  return x
+  
+couleur = ["trèfle", "carreau", "cœur", "pique"]
+
+carte = (valeur, couleur)
+
+"""
+
+#12 p 97
+"""[x for x in t if x>5]"""
+
+#19 p 97
+"""
+cette fonction va enumerer dans i et a m qui va enumerer dans j et b a si la valeur de x et b est equal alors i et j vaut l
+enumerate va enfaite creer une liste m avec comme element i puis a
+de meme a est une liste, donc m est une liste de liste, soit une matrice. 
+i va correspondre a la ligne de la matrice et j au numero de la colonne
+enumerate va donc retrouver le numero d'une ligne et d'une colonne pour retrouver un element d'une matrice
+"""
